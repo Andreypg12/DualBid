a82cb285da63dcdf4c20597d37450d2da4cafcc8
Andrey Perez <andreyperez109@gmail.com>
2026-01-26 20:57:19 -0600

    Add project files.

---
+++ DualBid/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DualBid.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
