35dcfe4d71b4bdad301b8656efa49da8ad4f76fc
Andrey Perez <andreyperez109@gmail.com>
2026-02-19 17:22:08 -0600

    Se agregó el repositorio de la imagen del comic

---
+++ DualBid.Infraestructure/Repository/Implementations/RepositoryImgComic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DualBid.Infraestructure.Data;
+using DualBid.Infraestructure.Models;
+using DualBid.Infraestructure.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DualBid.Infraestructure.Repository.Implementations
+{
+    public class RepositoryImgComic : IRepositoryImgComic
+    {
+        private readonly DualBidContext _context;
+
+        public RepositoryImgComic(DualBidContext Context)
+        {
+            this._context = Context;
+        }
+
+        public async Task<ICollection<ImgComic>> FindByComicIdAsync(int comicId)
+        {
+            var collection = await _context.Set<ImgComic>()
+                .Where(i => i.ComicId == comicId)
+                .AsNoTracking()
+                .ToListAsync();
+            return collection;
+        }
+    }
+}
