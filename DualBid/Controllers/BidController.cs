8361a0c0673791d2a02ab985908f39b7e7348807
Andrey Perez <andreyperez109@gmail.com>
2026-03-10 20:45:18 -0600

    Primer avance investigacion

--- DualBid/Controllers/BidController.cs
+++ DualBid/Controllers/BidController.cs
@@ -1,7 +1,11 @@
-﻿using DualBid.Application.Services.Implementations;
+﻿using DualBid.Application.DTOs;
+using DualBid.Application.Services.Implementations;
 using DualBid.Application.Services.Interfaces;
+using DualBid.Infraestructure.Models;
 using DualBid.ViewModels.Bid;
+using Libreria.Web.Util;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Mono.TextTemplating;
 
 namespace DualBid.Controllers
@@ -34,6 +38,69 @@ namespace DualBid.Controllers
             };
 
             return View(vm);
+        }
+
+        public ActionResult Create(int auctionId, int userId, string titleComicAuction, decimal minimunIncrease, decimal currentBidPrice)
+        {
+
+            var vm = new CreateBidViewModel
+            {
+                AuctionId = auctionId,
+                UserId = userId,
+                TitleComicAuction = titleComicAuction,
+                MinimunIncrease = minimunIncrease,
+                CurrentBidPrice = currentBidPrice
+            };
+
+            return View(vm);
+        }
+
+        // POST: LibroController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(CreateBidViewModel viewModeldto)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                // Recopilar todos los errores del ModelState
+                var errores = string.Join("<br>",
+                    ModelState.Values
+                        .SelectMany(v => v.Errors)
+                        .Select(e => e.ErrorMessage)
+                );
+
+                // Notificación SweetAlert con el detalle de errores
+                ViewBag.Notificacion = SweetAlertHelper.CrearNotificacion(
+                    "Errores de validación",
+                    $"El formulario contiene errores:<br>{errores}",
+                    SweetAlertMessageType.warning
+                );
+                return View(viewModeldto);
+            }
+
+            BidDTO dto = new()
+            {
+                AuctionId = viewModeldto.AuctionId,
+                UserId = viewModeldto.UserId,
+                AmountOffered = viewModeldto.AmountOffered,
+            };
+
+            await _serviceBid.AddAsync(dto);
+            //Notificar creación
+            TempData["Notificacion"] = SweetAlertHelper.CrearNotificacion(
+               "Libro creado correctamente",
+               $"El libro {dto.AmountOffered} fue registrado exitosamente.",
+               SweetAlertMessageType.success
+           );
+            return RedirectToAction(
+                "AuctionBiddingHistory",
+                "Bid",
+                new
+                {
+                    auctionId = viewModeldto.AuctionId,
+                    comicTitle = viewModeldto.TitleComicAuction
+                });
         }
     }
 }
---
+++ DualBid/ViewModels/Bid/CreateBidViewModel.cs
@@ -0,0 +1,15 @@
+﻿using DualBid.Application.DTOs;
+
+namespace DualBid.ViewModels.Bid
+{
+    public class CreateBidViewModel
+    {
+        public int AuctionId { get; set; }
+        public int UserId { get; set; }
+        public String TitleComicAuction { get; set; } = string.Empty;
+        public decimal MinimunIncrease { get; set; }
+        public decimal CurrentBidPrice { get; set; }
+
+        public decimal AmountOffered { get; set; }
+    }
+}
